c74419b3da3625f19c57b4522e44dffd7d564500
fisayo-dev <olufisayobadina@gmail.com>
2024-08-11 20:11:14 +0100

    chaged

--- dev/src/utils/PrivateRoutes.jsx
+++ dev/src/utils/PrivateRoutes.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const PrivateRoutes = () => {
-    const user = true;
+    const {user} = useAuth();
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
